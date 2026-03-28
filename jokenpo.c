42c460b45eb2f988172ed3d3d42c995232ce1b87
Nara <79164423+nararodriguess@users.noreply.github.com>
2026-03-28 13:21:43 +0000

    tratamento de erros

--- jokenpo.c
+++ jokenpo.c
@@ -15,59 +15,36 @@ int main () {
     scanf("%d", &escolhaJogador);
 
    
+    if( (escolhaJogador == 1) || (escolhaJogador == 2)|| (escolhaJogador == 3) ) {
 
-    if (escolhaJogador == 1 || escolhaJogador == 2 || escolhaJogador == 3) {
         escolhaComputador = rand() % 3 + 1;
-        
+
         switch(escolhaJogador) {
-        case 1:
-            printf("Jogador: Pedra - ");
-            break;
-        case 2:
-            printf("Jogador: Papel - ");
-            break;
-        case 3:
-            printf("Jogador: Tesoura - ");
-            break;
-        default:
-            printf("Opção inválida!\n");
-            break;
+            case 1: printf("Jogador: Pedra - ");    break;
+            case 2: printf("Jogador: Papel - ");    break;
+            case 3: printf("Jogador: Tesoura - ");  break;
         }
 
         switch(escolhaComputador) {
-        case 1:
-            printf("Computador: Pedra\n");
-            break;
-        case 2:
-            printf("Computador: Papel\n");
-            break;
-        case 3:
-            printf("Computador: Tesoura\n");
-            break;
+            case 1: printf("Computador: Pedra\n");    break;
+            case 2: printf("Computador: Papel\n");    break;
+            case 3: printf("Computador: Tesoura\n");  break;
         }
 
-
-        if (escolhaComputador == escolhaJogador) 
-        {
+        if (escolhaComputador == escolhaJogador) {
             printf("----- Jogo empatado! -----\n");
-        } 
-        else if (
-            (escolhaJogador == 1 && escolhaComputador == 3) || 
-            (escolhaJogador == 2 && escolhaComputador == 1) ||  
-            (escolhaJogador == 3 && escolhaComputador == 2) )  
-        {
-            printf("----- Jogador venceu! ---\n");
-        }     
-        else 
-        { 
-            printf("----- Computador venceu -----\n");
+        } else if (
+            (escolhaJogador == 1 && escolhaComputador == 3) ||
+            (escolhaJogador == 2 && escolhaComputador == 1) ||
+            (escolhaJogador == 3 && escolhaComputador == 2)
+        ) {
+            printf("----- Jogador venceu! -----\n");
+        } else {
+            printf("----- Computador venceu! -----\n");
         }
-    }
-    else 
-    { 
-        printf("Insira um valor válido para jogar.\n");
-    }
 
+    } else {
+        printf("Opção inválida! Encerrando o jogo.\n"); 
+    }
     return 0;
-
 }
